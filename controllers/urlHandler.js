3dcc7031c11350149be1bd53440a4443f3353428
Knight2 <8440420+knight2@users.noreply.github.com>
2018-12-14 12:38:12 -0500

    add env and gitignore

--- controllers/urlHandler.js
+++ controllers/urlHandler.js
@@ -1,5 +1,6 @@
 'use strict';
-
+var Counters = require('../models/counters.js');
+var UrlEntries = require('../models/urlEntries.js');
 var dns = require('dns');
 
 exports.addUrl = function(req, res){
