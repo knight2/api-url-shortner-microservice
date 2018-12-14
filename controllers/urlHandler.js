836b3edae2485ecf4f8989aeebc80bf8b29984a3
Knight2 <8440420+knight2@users.noreply.github.com>
2018-12-14 12:26:58 -0500

    added controller - urlHandler

---
+++ controllers/urlHandler.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var dns = require('dns');
+
+exports.addUrl = function(req, res){
+    res.json({addUrl: "Incomplete"});
+};
+
+exports.processShortUrl = function(req, res){
+    res.json({processShortUrl: "Incomplete"});
+};
