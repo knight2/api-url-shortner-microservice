660a18e4f7ea4cca40befbe79897238cf86f001a
Knight2 <8440420+knight2@users.noreply.github.com>
2018-12-14 12:42:50 -0500

    add dotenv, bodyparser, mongoose connection string

--- server.js
+++ server.js
@@ -3,8 +3,10 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+require('dotenv').config();
 
 var cors = require('cors');
+var bodyParser = require('body-parser');
 
 var urlHandler = require('./controllers/urlHandler.js');
 
@@ -13,13 +15,13 @@ var app = express();
 // Basic Configuration 
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
-
-app.use(cors());
+mongoose.connect(process.env.MONGOLAB_URI);
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+
+app.use(cors());
+app.use(bodyParser.urlencoded({'extended': false}));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -27,7 +29,6 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
